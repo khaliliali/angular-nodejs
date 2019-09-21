686606dcbec276f6db3d6ebfbd211d369e33d9f6
khaliliali <ali.khalili@outlook.com>
2019-09-21 12:43:56 +0800

    Code Optimization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
   {
     path: 'edit/:postId',
     component: PostCreateComponent,
     canActivate: [AuthGuard]
-  },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  }
 ];
 
 @NgModule({
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -6,6 +6,8 @@ import { Router } from '@angular/router';
 
 import { Post } from './post.model';
 
+const BACKEND_URL = 'http://localhost:3000/api/posts';
+
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
@@ -17,7 +19,7 @@ export class PostsService {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
       .get<{ message: string; posts: any; maxPosts: number }>(
-        'http://localhost:3000/api/posts' + queryParams
+        BACKEND_URL + queryParams
       )
       .pipe(
         map(postData => {
@@ -55,7 +57,7 @@ export class PostsService {
       content: string;
       imagePath: string;
       creator: string;
-    }>('http://localhost:3000/api/posts/' + id);
+    }>(BACKEND_URL + id);
   }
 
   addPost(title: string, content: string, image: File) {
@@ -90,14 +92,12 @@ export class PostsService {
         creator: null
       };
     }
-    this.http
-      .put('http://localhost:3000/api/posts/' + id, postData)
-      .subscribe(response => {
-        this.router.navigate(['/']);
-      });
+    this.http.put(BACKEND_URL + id, postData).subscribe(response => {
+      this.router.navigate(['/']);
+    });
   }
 
   deletePost(postId: string) {
-    return this.http.delete('http://localhost:3000/api/posts/' + postId);
+    return this.http.delete(BACKEND_URL + postId);
   }
 }
